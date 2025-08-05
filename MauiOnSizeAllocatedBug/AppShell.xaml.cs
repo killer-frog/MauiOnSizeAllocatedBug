8f35cfcc5ad9dc7deeb7ff04fcd5e0a81d8b75e2
ash <ash@killerfrog.co.uk>
2025-08-05 12:15:05 +0100

    Creatre Project on Repo

--- MauiOnSizeAllocatedBug/AppShell.xaml.cs
+++ MauiOnSizeAllocatedBug/AppShell.xaml.cs
@@ -1,9 +1,24 @@
-﻿namespace MauiOnSizeAllocatedBug;
+﻿using System.Diagnostics;
+
+namespace MauiOnSizeAllocatedBug;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    public AppShell()
+    {
+        InitializeComponent();
+        Debug.WriteLine("*** AppShell ctor");
+    }
+
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        Debug.WriteLine($"*** Size allocated (w:h): {width}x{height}");
+        base.OnSizeAllocated(width, height);
+    }
+
+    protected override void ChangeVisualState()
+    {
+        base.ChangeVisualState();
+        Debug.WriteLine($"*** Change VisualState (w x h) {Width}x{Height}");
+    }
 }
